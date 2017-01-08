5e350afd84d9c418dbb2b937eba57ff56a6953e2
Sara Bee <sara.bainbridge@gmail.com>
2017-01-08 17:40:05 -0500

    Hook up HTU21D sensor

--- src/creeper/creeper.ino
+++ src/creeper/creeper.ino
@@ -1,12 +1,18 @@
 #include <VirtualWire.h>
+#include "SparkFun_Si7021_Breakout_Library.h"
 
 #define LED_PIN 13
 #define PHOTOCELL_PIN A0
+
+// HTU21D temp/humidity sensor
+Weather sensor;
 
 int count = 1;
 
 void setup() {
   Serial.begin(9600);
+
+  sensor.begin();
 
   pinMode(LED_PIN, OUTPUT);
   pinMode(PHOTOCELL_PIN, INPUT);
@@ -20,6 +26,11 @@ void loop(){
   int lightADC = analogRead(PHOTOCELL_PIN);
   Serial.println(lightADC);
 
+  float humidity = sensor.getRH();
+  float tempf = sensor.getTempF();
+
+  Serial.println(humidity);
+  Serial.println(tempf);
 
   char msg[7] = {'h','e','l','l','o',' ','#'};
   msg[6] = '0' + count;
