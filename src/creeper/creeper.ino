fc07db2b32adc04056ed4ae4e9627250d2ceb384
Sara Bee <sara.bainbridge@gmail.com>
2017-01-08 18:00:29 -0500

    :satellite: Sensor readings

--- src/creeper/creeper.ino
+++ src/creeper/creeper.ino
@@ -23,24 +23,37 @@ void setup() {
 }
 
 void loop(){
+  unsigned long timestamp = millis();
+  Serial.print("t: ");
+  Serial.println(timestamp);
+
   int lightADC = analogRead(PHOTOCELL_PIN);
+  Serial.print("L: ");
   Serial.println(lightADC);
 
   float humidity = sensor.getRH();
+  char humidityStr[16];
+  dtostrf(humidity, 5, 2, humidityStr);
+  Serial.print("H: ");
+  Serial.println(humidityStr);
+
   float tempf = sensor.getTempF();
+  char tempStr[16];
+  dtostrf(tempf, 5, 2, tempStr);
+  Serial.print("T: ");
+  Serial.println(tempStr);
 
-  Serial.println(humidity);
-  Serial.println(tempf);
-
-  char msg[7] = {'h','e','l','l','o',' ','#'};
-  msg[6] = '0' + count;
-  Serial.println((char*)msg);
-
-  digitalWrite(13, HIGH);
-  vw_send((uint8_t *)msg, 7);
-  vw_wait_tx();
-  digitalWrite(13, LOW);
-  delay(1000);
-  count = count + 1;
   Serial.println();
+
+  // char msg[7] = {'h','e','l','l','o',' ','#'};
+  // msg[6] = '0' + count;
+  // Serial.println((char*)msg);
+
+  // digitalWrite(13, HIGH);
+  // vw_send((uint8_t *)msg, 7);
+  // vw_wait_tx();
+  // digitalWrite(13, LOW);
+  // delay(1000);
+  // count = count + 1;
+  // Serial.println();
 }
