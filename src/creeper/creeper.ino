6c0930eca948d666bf35348e75769e7962695e53
Sara Bee <sara.bainbridge@gmail.com>
2017-01-08 22:00:31 -0500

    Use structs for packets

--- src/creeper/creeper.ino
+++ src/creeper/creeper.ino
@@ -1,14 +1,11 @@
 #include <VirtualWire.h>
 #include "SparkFun_Si7021_Breakout_Library.h"
+#include "../Creepers.h"
 
 #define LED_PIN 13
 #define PHOTOCELL_PIN A0
 
-#define PACKET_SIZE 14 // bytes
-
-void logData(unsigned long timestamp, int light, float humidity, float tempf);
-
-uint8_t packet[14];
+void logData(packet p);
 
 // HTU21D temp/humidity sensor
 Weather sensor;
@@ -28,57 +25,42 @@ void setup() {
 }
 
 void loop() {
-  unsigned long timestamp = millis(); // 4 bytes
-  int lightADC = analogRead(PHOTOCELL_PIN); // 2 bytes
-  float humidity = sensor.getRH(); // 4 bytes
-  float tempf = sensor.getTempF(); // 4 bytes
 
-  logData(timestamp, lightADC, humidity, tempf);
+  struct packet outbound;
+  outbound.timestamp = millis();
+  outbound.light = analogRead(PHOTOCELL_PIN);
+  outbound.humidity = (int)(sensor.getRH() * 100);
 
-  // Serialize the packet to an array of uint8_t so it can be sent via RF
-  packet[0] = (uint8_t)((timestamp >> 24) & 0xFF);
-  packet[1] = (uint8_t)((timestamp >> 16) & 0xFF);
-  packet[2] = (uint8_t)((timestamp >> 8) & 0xFF);
-  packet[3] = (uint8_t)(timestamp & 0xFF);
+  float tempf = sensor.getTempF();
+  outbound.tempc = (int)((tempf - 32)/1.8 * 100);
 
-  packet[4] = (uint8_t)((lightADC >> 8) & 0xFF);
-  packet[5] = (uint8_t)(lightADC & 0xFF);
+  logData(outbound);
 
-  packet[6] = 0; // (uint8_t)((humidity >> 24) & 0xFF);
-  packet[7] = 0; // (uint8_t)((humidity >> 16) & 0xFF);
-  packet[8] = 0; // (uint8_t)((humidity >> 8) & 0xFF);
-  packet[9] = 0; // (uint8_t)(humidity & 0xFF);
-
-  packet[10] = 0; // = 0; // (uint8_t)((tempf >> 24) & 0xFF);
-  packet[11] = 0; // (uint8_t)((tempf >> 16) & 0xFF);
-  packet[12] = 0; // (uint8_t)((tempf >> 8) & 0xFF);
-  packet[13] = 0; // (uint8_t)(tempf & 0xFF);
-
-  Serial.println((char*) packet);
+  char b[sizeof(outbound)];
+  memcpy(b, &outbound, sizeof(outbound));
 
   digitalWrite(13, HIGH);
-  vw_send(packet, 4);
+
+  vw_send((char*)b, sizeof(b));
   vw_wait_tx();
+
   digitalWrite(13, LOW);
+
   delay(1000);
 }
 
-void logData(unsigned long timestamp, int light, float humidity, float tempf) {
+void logData(packet p) {
   Serial.print("t: ");
-  Serial.println(timestamp);
+  Serial.println(p.timestamp);
 
   Serial.print("L: ");
-  Serial.println(light);
+  Serial.println(p.light);
 
-  char humidityStr[16];
-  dtostrf(humidity, 5, 2, humidityStr);
   Serial.print("H: ");
-  Serial.println(humidityStr);
+  Serial.println(p.humidity);
 
-  char tempStr[16];
-  dtostrf(tempf, 5, 2, tempStr);
   Serial.print("T: ");
-  Serial.println(tempStr);
+  Serial.println(p.tempc);
   Serial.println();
 }
 
--- src/taproot/taproot.ino
+++ src/taproot/taproot.ino
@@ -1,4 +1,5 @@
 #include <VirtualWire.h>
+#include "../Creepers.h"
 
 void setup() {
   Serial.begin(9600);
@@ -6,9 +7,9 @@ void setup() {
 
   pinMode(13, OUTPUT);
 
-  // vw_set_ptt_inverted(true);
   vw_set_rx_pin(12);
   vw_setup(4000); // bps
+
   // Start the receiver PLL running
   vw_rx_start();
 }
@@ -18,29 +19,14 @@ void loop() {
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
 
   if (vw_get_message(buf, &buflen)) {
-    unsigned long timestamp;
-    uint8_t tbuff[] = { buf[3], buf[2], buf[1], buf[0] };
-    memcpy(&timestamp, &tbuff, sizeof(timestamp));
-    Serial.print("t: ");
-    Serial.println(timestamp);
 
-    int light;
-    uint8_t lbuff[] = { buf[5], buf[4] };
-    memcpy(&light, &lbuff, sizeof(light));
-    Serial.print("L: ");
-    Serial.println(light);
+    packet inbound;
+    memcpy(&inbound, buf, sizeof(inbound));
 
-    float humidity;
-    uint8_t hbuff[] = { buf[9], buf[8], buf[7], buf[6] };
-    memcpy(&humidity, &hbuff, sizeof(humidity));
-    Serial.print("H: ");
-    Serial.println(humidity);
-
-    float temperature;
-    uint8_t pbuff[] = { buf[13], buf[12], buf[11], buf[10] };
-    memcpy(&temperature, &pbuff, sizeof(temperature));
-    Serial.print("L: ");
-    Serial.println(temperature);
+    Serial.println(inbound.timestamp);
+    Serial.println(inbound.light);
+    Serial.println(inbound.humidity);
+    Serial.println(inbound.tempc);
 
     Serial.println();
   }
